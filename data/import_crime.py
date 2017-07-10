181287d3383f092b35f1cfc19cc8c533d0e6b33a
Matt Policastro <matt@mattpolicastro.com>
2017-07-09 20:31:42 -0400

    Re-scaled crime report counts
    Did some exploratory data analysis and realised the first half of 2010
    was pretty much empty. Ran a simple linear regression to get an estimate
    for the total counts (model formula and results are included in
    `analysis/report.Rmd`). Updated the source and output data files.
    
    Also improved the data import script to take the year as an argument.

--- data/import_crime.py
+++ data/import_crime.py
@@ -4,6 +4,7 @@ Download Cincinnati Police Department data from the city's open data portal.
 
 import json
 from os import path
+import sys
 
 import pandas as pd
 from sodapy import Socrata
@@ -22,17 +23,21 @@ CLIENT = Socrata(
     CONFIG['password']
 )
 
+
+YEAR = str(sys.argv[1])
+DATE_RANGE = '"' + YEAR +'-01-01T00:00:00" and "' + YEAR + '-12-31T23:59:59"'
+
 # Download 2010 crime data and convert to DataFrame
 # API endpoint:
 # https://data.cincinnati-oh.gov/resource/4qzi-nepn.json
 CRIMES = pd.DataFrame(CLIENT.get(
     '4qzi-nepn',
-    where='occurredon between "2010-01-01T00:00:00" and "2010-12-31T23:59:59"',
-    order='occurredon ASC',
+    where='reportedon between ' + DATE_RANGE + ' AND occurredon between ' + DATE_RANGE,
+    order='reportedon ASC',
     # Manually upping limit so data isn't truncated
     limit='50000'
 ))
 CLIENT.close()
 
 # Write to file
-CRIMES.to_csv(path.join(DIR, 'crimes_raw.csv'), index=False)
+CRIMES.to_csv(path.join(DIR, 'raw', 'crimes_raw_' + YEAR + '.csv'), index=False)
